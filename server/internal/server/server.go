1b2846bceab56109c86594ec6012a48adc86f147
Your Name <you@example.com>
2023-12-09 21:31:11 +0300

    fix: fix cors try 1

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -12,7 +12,7 @@ func InitServer() {
 
 	app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"https://url-shortener-dusky-zeta.vercel.app/"},
-		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowHeaders:     []string{"*"},
 		AllowMethods:     []string{"GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
 	}))
